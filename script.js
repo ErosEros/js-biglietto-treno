3a1032aed7b14fca3feb0bc6120965481707851a
ErosEros <eros.vittori1994@gmail.com>
2022-08-23 18:27:32 +0200

    created html e js + test for operation

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("js ok!")
+
+// first way to say: Ciao a tutti (print in html)
+document.writeln('Ciao a tutti')
+
+// second way to say: Ciao a tutti (print on the page)
+alert('Ciao a tutti')
+
