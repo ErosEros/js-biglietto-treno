f825f5749e9d8a78af05e68ad9d8b007c577ef5a
ErosEros <eros.vittori1994@gmail.com>
2022-08-23 18:48:59 +0200

    first question: età del passeggero

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-console.log("js ok!")
+// console.log("js ok!")
 
-// first way to say: Ciao a tutti (print in html)
-document.writeln('Ciao a tutti')
+// // first way to say: Ciao a tutti (print in html)
+// document.writeln('Ciao a tutti')
 
-// second way to say: Ciao a tutti (print on the page)
-alert('Ciao a tutti')
+// // second way to say: Ciao a tutti (print on the page)
+// alert('Ciao a tutti')
 
+const etaDelPasseggero = prompt ('Quanti anni hai?')
+const textAsNumber = parseInt ('etaDelPasseggero')
+console.log (etaDelPasseggero)
