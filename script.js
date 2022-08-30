27a4592b562e6d7626818a1f0c4a9069cf2765f2
ErosEros <eros.vittori1994@gmail.com>
2022-08-30 18:18:40 +0200

    adjustments

--- script.js
+++ script.js
@@ -8,27 +8,26 @@
 
 // first question
 const etaDelPasseggero = prompt ('Quanti anni hai?');
-const textAsNumber = parseInt ('etaDelPasseggero');
-console.log (etaDelPasseggero);
+const textAsNumber = parseInt (etaDelPasseggero);
+console.log (textAsNumber);
 // second question
 const km =prompt ('Quanti km devi percorrere?');
 const Number = parseInt (km);
-console.log (km)
+console.log (km);
 
 // price for ticket
 let ticket = km * 0.21;
 
 // condition
 if (textAsNumber < 18) {
-    let ventiPercento  = (ticket / 100) * 20;
+    let ventiPercento  = (ticket / 100) *20;
     ticket = ticket - ventiPercento;
 }
 
 else if (textAsNumber > 65) {
-    let quarantaPercento  = (ticket / 100) * 40;
+    let quarantaPercento  = (ticket / 100) *40;
     ticket = ticket - quarantaPercento;
 }
 
 
-
-document.getElementById('ErosId').innerHTML = 'price for ticket'
+document.getElementById('biglietto').innerHTML = 'price for ticket' + " " + ticket 
